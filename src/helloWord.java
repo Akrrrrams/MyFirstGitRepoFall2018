3c2cfc801a8bf01b77a820f9ad56c44919e19786
Akram <akins7559@gmail.com>
2019-01-16 21:33:29 -0500

    Second commit . Good Even, JavaClass

--- src/helloWord.java
+++ src/helloWord.java
@@ -1,5 +1,6 @@
 public class helloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("hello word");
+        System.out.println("Good Evening");
     }
 }
